a660a98466eae02e76e7e865426fe53f35dfa7b9
Yeasir Arefin Tusher <yeasir.arefin@bjitacademy.com>
2023-01-17 09:02:06 +0600

    Sd WebImage: Working
    Fetch request working

--- The News Explorer/Model/Coredata/CDArticle+CoreDataProperties.swift
+++ The News Explorer/Model/Coredata/CDArticle+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  CDArticle+CoreDataProperties.swift
 //  The News Explorer
 //
-//  Created by Yeasir Arefin Tusher on 15/1/23.
+//  Created by bjit on 17/1/23.
 //
 //
 
@@ -16,13 +16,14 @@ extension CDArticle {
         return NSFetchRequest<CDArticle>(entityName: "CDArticle")
     }
 
-    @NSManaged public var seourceName: String?
     @NSManaged public var author: String?
-    @NSManaged public var title: String?
+    @NSManaged public var content: String?
     @NSManaged public var descriptionText: String?
     @NSManaged public var newsUrl: String?
     @NSManaged public var publishedDate: Date?
-    @NSManaged public var content: String?
+    @NSManaged public var seourceName: String?
+    @NSManaged public var title: String?
+    @NSManaged public var imageUrl: String?
     @NSManaged public var category: CDCategory?
 
 }
@@ -30,4 +31,3 @@ extension CDArticle {
 extension CDArticle : Identifiable {
 
 }
-
