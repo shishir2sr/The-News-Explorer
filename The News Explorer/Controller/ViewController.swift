0df0469f7bd748827bdb23fb352a0807a01aff08
Yeasir Arefin Tusher <yeasir.arefin@bjitacademy.com>
2023-01-17 13:05:12 +0600

    Done

--- The News Explorer/Controller/ViewController.swift
+++ The News Explorer/Controller/ViewController.swift
@@ -7,16 +7,32 @@ class ViewController: UIViewController {
     
     var  articles: [Article] = []
     var isLoading = false
+
     
     // MARK: FetchedResultController
     lazy var fetchedhResultController: NSFetchedResultsController<NSFetchRequestResult> = {
+        
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: String(describing: CDArticle.self))
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "publishedDate", ascending: true)]
         fetchRequest.fetchBatchSize = 10
         let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.sharedInstance.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
         frc.delegate = self
         return frc
+        
     }()
+    
+    lazy var fetchedhResultController2: NSFetchedResultsController<NSFetchRequestResult> = {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: String(describing: CDArticle.self))
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "publishedDate", ascending: true)]
+        fetchRequest.predicate = NSPredicate(format: "category.categoryName == %@", "business")
+        fetchRequest.fetchBatchSize = 10
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.sharedInstance.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        frc.delegate = self
+        return frc
+    }()
+    
+    
+    
     
     
     // MARK: Outlets
@@ -25,7 +41,7 @@ class ViewController: UIViewController {
     
     
     //MARK: ViewDidLoad method
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         firstLaunch()
@@ -34,6 +50,41 @@ class ViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
     }
+
+    //MARK: First launch logic
+    fileprivate func firstLaunch() {
+        let hasLaunchedBefore = UserDefaults.standard.bool(forKey: "hasLaunchedBefore")
+        print(hasLaunchedBefore)
+        
+        if !hasLaunchedBefore {
+            addCategories()
+            coreDataInit(){
+                try? self.fetchedhResultController.performFetch()
+                self.isLoading = false
+                self.tableView.reloadData()
+            }
+            UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
+            
+        } else{
+            try? self.fetchedhResultController.performFetch()
+            self.isLoading = false
+            self.tableView.reloadData()
+        }
+    }
+    
+    // reload table data
+    
+//    func reloadTableData(collectionViewIndexPath: Int) {
+//        self.collectionVCIndexPath = collectionViewIndexPath
+//        do {
+//            try self.fetchedhResultController.performFetch()
+//        } catch {
+//            print("Error fetching data: \(error)")
+//        }
+//        self.tableView.reloadData()
+//    }
+//
+    
     
     // MARK: CoreData Init()
     func coreDataInit(completion: @escaping  ()->Void) {
@@ -58,31 +109,7 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    //MARK: First launch logic
-    fileprivate func firstLaunch() {
-        let hasLaunchedBefore = UserDefaults.standard.bool(forKey: "hasLaunchedBefore")
-        print(hasLaunchedBefore)
-        if !hasLaunchedBefore {
-            addCategories()
-            coreDataInit(){
-                try? self.fetchedhResultController.performFetch()
-                self.isLoading = false
-                self.tableView.reloadData()
-            }
-            UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
-            
-        } else{
-            try? self.fetchedhResultController.performFetch()
-            self.isLoading = false
-            self.tableView.reloadData()
-        }
-    }
-    
 }
-
-
-
 
 //MARK: Create Categories
 func addCategories() {
@@ -151,6 +178,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
         
         if !isLoading{
             let article = fetchedhResultController.object(at: indexPath) as! CDArticle
+            
             cell.newsTitle.text = article.title
             cell.newsSource.text = article.seourceName
             cell.newsPublishedData.text = article.publishedDate?.formatted()
@@ -161,15 +189,23 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
 
 //MARK: - Collectionview Delegate and Data source
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    // MARK: Number of items in section
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         Constants.categoryModelList.count
     }
     
+    //MARK: Cell for item at
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let category = Constants.categoryModelList[indexPath.row]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.categoryCVCell, for: indexPath) as! CategoryCollectionVC
@@ -179,9 +215,26 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
         return cell
     }
     
-    
+    // MARK: DidSelectItem At
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+       
+        _ = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.categoryCVCell, for: indexPath) as! CategoryCollectionVC
+        
         print(indexPath.row)
+        isLoading = false
+        
+        do{
+            let categoryName = Constants.categoryModelList[indexPath.row].categoryName
+            
+            if indexPath.row > 0{fetchedhResultController.fetchRequest.predicate = NSPredicate(format: "category.categoryName == %@", categoryName)}
+        
+            try fetchedhResultController.performFetch()
+            
+        }catch{
+            print(error.localizedDescription)
+        }
+        tableView.reloadData()
+        
     }
 }
 
