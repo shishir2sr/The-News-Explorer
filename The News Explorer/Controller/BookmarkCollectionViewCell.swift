0bda539e9e0b324efd7528b40fc58d81182a8a65
Yeasir Arefin Tusher <tusher15-5944@diu.edu.bd>
2023-01-18 23:35:00 +0600

    Bookmark: UI added
    UI done with tableview and collectionview

---
+++ The News Explorer/Controller/BookmarkCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BookmarkCollectionViewCell.swift
+//  The News Explorer
+//
+//  Created by Yeasir Arefin Tusher on 18/1/23.
+//
+
+import UIKit
+
+class BookmarkCollectionViewCell: UICollectionViewCell {
+    
+}
--- The News Explorer/Controller/BookmarkViewController.swift
+++ The News Explorer/Controller/BookmarkViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class BookmarkViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var searchField: UITextField!
+    
+    @IBOutlet weak var categoryName: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
 
+}
+
+extension BookmarkViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.homeTableViewCell, for: indexPath) as! HomeTableViewCell
+        cell.newsTitle.text = "Nothing"
+        cell.newsImage.image = UIImage(named: "placeholder")
+        return cell
+    }
+    
+    
 }
